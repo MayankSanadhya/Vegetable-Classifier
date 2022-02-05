71bd04cb5d95be90d6fc9297efe4f6ca50964317
Mayank Sanadhya <95864691+MayankSanadhya@users.noreply.github.com>
2022-02-05 15:58:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = load_model('cnn.h5')
+    return model
+model = load_model()
+st.write("""
+          # Vegetable Prediction
+         """
+        )
+file = st.file_uploader("Please upload an vegetable image",type=['jpg'])
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    
+    size = (64,64)
+    image = ImageOps,fit(image_data,size,Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    
+    return prediction
+if file is None:
+    st.text("Please Upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image,use_column,width=True)
+    predictions = import_and_predict(image,model)
+    class_names = ['Bean','Bitter_Gourd','Bottle_Gourd','Brinjal','Broccoli','Cabbage','Capsicum','Carrot','Cauliflower','Cucumber','Papaya','Potato','Pumpkin','Radish','Tomato']
+    string = "This image most likely is : "+ class_names[np.argmax(predictions)]
+    st.success(string)
